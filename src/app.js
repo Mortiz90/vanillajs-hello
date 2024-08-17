925388a8612c5691eea16730cabaddaa388fb4f6
Mauricio Reinales <146978569+Mortiz90@users.noreply.github.com>
2024-08-17 15:58:15 +0000

    ada

--- src/app.js
+++ src/app.js
@@ -7,5 +7,42 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  const domains = [
+    {
+      pronoun: ["the", "our", "tu"]
+    },
+    {
+      adj: ["great", "big", "también"]
+    },
+    {
+      noun: ["jogger", "racoon", "puedes"]
+    },
+    {
+      dom: [".com", ".net", ".es", ".edu", ".org"]
+    }
+  ];
+
+  let domainHTML = '<ul class="list-group">';
+
+  for (let pronoun of domains[0].pronoun) {
+    for (let adj of domains[1].adj) {
+      for (let noun of domains[2].noun) {
+        for (let dom of domains[3].dom) {
+          let domain;
+          if (dom === ".es" && noun === "puedes") {
+            domain = `${pronoun}${adj}pued${dom}`;
+          } else {
+            domain = `${pronoun}${adj}${noun}${dom}`;
+          }
+          domainHTML += `<li class="list-group-item">${domain}</li>`;
+        }
+      }
+    }
+  }
+
+  domainHTML += "</ul>";
+
+  const dominios = document.querySelector("#domains");
+  dominios.innerHTML = domainHTML;
 };
